7ed3df6fc2ba95559e4a8aa857856ba3a9a469c1
AhmedBhiri <ahmedbhiri93@gmail.com>
2023-01-28 17:57:05 +0100

    Dynamic Cart & better styles

--- src/components/PlantItem.jsx
+++ src/components/PlantItem.jsx
@@ -1,13 +1,11 @@
 import CareScale from './CareScale'
 import '../styles/PlantItem.css'
 
-function handleClick(plantName) {
-    alert(`Vous voulez acheter 1 ${plantName} ? Très bon choix 🌱✨`)
-}
+
 
 function PlantItem({ id, cover, name, water, light, price, categories }) {
 	return (
-		<li  className='lmj-plant-item' onClick={() =>handleClick(name)}>
+		<li  className='lmj-plant-item'>
 			<span className='lmj-plant-item-price'>{price}€</span>
 			<img className='lmj-plant-item-cover' src={cover} alt={`${name} cover`} />
 			<div className='lmj-plant-item-name'>
